3307475da9e1299cd6b474b875714b3f7481b7e4
Vladimir <hotsru12@gmail.com>
2022-10-23 01:16:30 +0300

    process

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController, CommentController } from './controllers/index.js';
 
 mongoose
-  .connect('mongodb+srv://admin:dm7yvAgUzJfARrYG@cluster0.m8lgzml.mongodb.net/?retryWrites=true&w=majority')
+  .connect('process.env.MONGODB_URI')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
